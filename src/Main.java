da538c6282c126eda16804d6d2cb010a7a39511d
mdaurbekov <dreamkov@gmail.com>
2022-09-02 23:23:57 +0300

    stream api

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John", "Филе");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown", "Куриное");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        long count = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.printf("Количество несовершеннолетних: %s \n", count);
+
+        List<String> list = persons.stream()
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> person.getAge() < 27)
+                .map(Person::getName)
+                .toList();
+
+        System.out.println("Призывники:");
+        System.out.println(list);
+
+        List<String> workables = persons.stream()
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> person.getSex().equals(Sex.MAN) & person.getAge() < 65 || person.getSex().equals(Sex.WOMAN) & person.getAge() < 60)
+                .map(Person::getName)
+                .sorted()
+                .toList();
+
+        System.out.println("Трудоспособного возраста:");
+        System.out.println(workables);
+
+    }
+}
